913905f26801cc75815f066a2a63db01ee4fa58a
Fedeya <elfedeminaya@gmail.com>
2020-03-29 15:42:07 -0300

    added delete project type

--- client/src/types/index.js
+++ client/src/types/index.js
@@ -2,3 +2,4 @@ export const OPEN_FORM = 'OPEN_FORM';
 export const GET_PROJECTS = 'GET_PROJECTS';
 export const ADD_PROJECT = 'ADD_PROJECT';
 export const ACTIVE_PROJECT = 'ACTIVE_PROJECT';
+export const DELETE_PROJECT = 'DELETE_PROJECT';
