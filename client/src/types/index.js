12944bc2c22626fe978cc7e89ea6525678156559
Fedeya <elfedeminaya@gmail.com>
2020-03-29 16:20:07 -0300

    type tasks project added

--- client/src/types/index.js
+++ client/src/types/index.js
@@ -3,3 +3,5 @@ export const GET_PROJECTS = 'GET_PROJECTS';
 export const ADD_PROJECT = 'ADD_PROJECT';
 export const ACTIVE_PROJECT = 'ACTIVE_PROJECT';
 export const DELETE_PROJECT = 'DELETE_PROJECT';
+
+export const TASKS_PROJECT = 'TASKS_PROJECT';
