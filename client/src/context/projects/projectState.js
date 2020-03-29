8cfb5cf09f767cc06a6676957172ec04e925bc71
Fedeya <elfedeminaya@gmail.com>
2020-03-29 04:29:43 -0300

    added projects scaffold to initial state

--- client/src/context/projects/projectState.js
+++ client/src/context/projects/projectState.js
@@ -3,20 +3,37 @@ import React, { useReducer } from 'react';
 import projectContext from './projectContext';
 import projectReducer from './projectReducer';
 
-function projectState(props) {
+import { OPEN_FORM } from '../../types';
+
+function ProjectState(props) {
   const initialState = {
-    openForm: false
+    openForm: false,
+    projects: [
+      { id: 1, name: 'Tienda Virtual' }, 
+      { id: 2, name: 'Web Design' }, 
+      { id: 3, name: 'Mobile App' }
+    ]
   };
 
   const [state, dispatch] = useReducer(projectReducer, initialState);
 
+  const showForm = () => {
+    dispatch({
+      type: OPEN_FORM
+    });
+  }
+
   return (
     <projectContext.Provider
       value={{
-        openForm: state.openForm
+        openForm: state.openForm,
+        projects: state.projects,
+        showForm
       }}
     >
       {props.children}
     </projectContext.Provider>
   );
 }
+
+export default ProjectState;
