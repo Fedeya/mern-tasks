0c73a5ced8fa389773b4203bf8e1304eb4f2633c
Fedeya <elfedeminaya@gmail.com>
2020-03-29 02:16:52 -0300

    Updated FormTask Scaffold

--- client/src/components/tasks/FormTask.js
+++ client/src/components/tasks/FormTask.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 function FormTask() {
   return (
-    <h1>Form Works!</h1>
+    <div className="form">
+      <form>
+        <div className="container-input">
+          <input 
+            type="text"
+            name="name"
+            className="input-text"
+            placeholder="Task Name"
+          />
+        </div>
+        <div className="container-input">
+          <button className="btn btn-primary btn-submit btn-block">
+            Add Task
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
 
