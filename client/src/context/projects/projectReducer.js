d2ce1a97e55af11ebc4705c5ed56e9632032e2cc
Fedeya <elfedeminaya@gmail.com>
2020-03-29 04:26:20 -0300

    defined action open_form for show form

--- client/src/context/projects/projectReducer.js
+++ client/src/context/projects/projectReducer.js
@@ -1,5 +1,12 @@
+import { OPEN_FORM } from '../../types';
+
 export default (state, action) => {
   switch(action.type) {
+    case OPEN_FORM: 
+      return {
+        ...state,
+        openForm: !state.openForm
+      }
     default:
       return state;
   }
