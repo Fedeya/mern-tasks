027e04e2e28b178bbe382851b54524defbc3474f
Fedeya <elfedeminaya@gmail.com>
2020-03-29 15:35:45 -0300

    show form for create task if project is not a null

--- client/src/components/tasks/FormTask.js
+++ client/src/components/tasks/FormTask.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import projectContext from '../../context/projects/projectContext';
 
 function FormTask() {
+
+  const { project } = useContext(projectContext);
+
+  if(!project) return null;
+
   return (
     <div className="form">
       <form>
