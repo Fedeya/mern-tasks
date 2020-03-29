8590a48f40f6eca2c457a5ad7d0637a9a80c4488
Fedeya <elfedeminaya@gmail.com>
2020-03-29 15:24:03 -0300

    used the activeProject func of project context state

--- client/src/components/projects/Project.js
+++ client/src/components/projects/Project.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import projectContext from '../../context/projects/projectContext';
 
 function Project({ project }) {
+
+  const { activeProject } = useContext(projectContext);
+
   return (
     <li>
       <button
         className="btn btn-blank"
+        onClick={() => activeProject(project)}
       >
         {project.name}
       </button>
