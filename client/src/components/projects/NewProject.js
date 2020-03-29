7ce031f42bdf1095ef56284b4c1806772ecf6a26
Fedeya <elfedeminaya@gmail.com>
2020-03-29 01:48:01 -0300

    added state and submit form funcs in new project

--- client/src/components/projects/NewProject.js
+++ client/src/components/projects/NewProject.js
@@ -1,18 +1,27 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 function NewProject() {
+
+  const [name, setName] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  }
+
   return (
     <Fragment>
       <button className="btn btn-block btn-primary">
         New Project
       </button>
 
-      <form className="form-new-project" >
+      <form className="form-new-project" onSubmit={handleSubmit} >
         <input 
           type="text" 
           name="name"
           className="input-text"
           placeholder="Project Name"
+          value={name}
+          onChange={e => setName(e.target.value)}
         />
 
         <button className="btn btn-primary btn-block">
