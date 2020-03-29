03fe188c53e4208e3ec2517365172de7ff718b6f
Fedeya <elfedeminaya@gmail.com>
2020-03-29 01:35:05 -0300

    created sidebar

---
+++ client/src/components/layout/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Sidebar() {
+  return (
+    <aside>
+      <h1>MERN<span>Tasks</span></h1>
+      <div className="projects">
+        <h2>Your Projects</h2>
+      </div>
+    </aside>
+  );
+}
+
+export default Sidebar;
