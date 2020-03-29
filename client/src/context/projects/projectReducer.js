5c5bdbe0ef54bd2e1e60d18379064674cb145c02
Fedeya <elfedeminaya@gmail.com>
2020-03-29 15:44:02 -0300

    added delete project reducer

--- client/src/context/projects/projectReducer.js
+++ client/src/context/projects/projectReducer.js
@@ -1,4 +1,4 @@
-import { OPEN_FORM, GET_PROJECTS, ADD_PROJECT, ACTIVE_PROJECT } from '../../types';
+import { OPEN_FORM, GET_PROJECTS, ADD_PROJECT, ACTIVE_PROJECT, DELETE_PROJECT } from '../../types';
 
 export default (state, action) => {
   switch(action.type) {
@@ -22,6 +22,12 @@ export default (state, action) => {
       return {
         ...state,
         project: action.payload
+      }
+    case DELETE_PROJECT:
+      return {
+        ...state,
+        projects: state.projects.filter(project => project.id !== action.payload),
+        project: null
       }
     default:
       return state;
