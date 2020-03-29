98375e5608c8a1f4153d897420f08b4125af7fcd
Fedeya <elfedeminaya@gmail.com>
2020-03-29 17:05:48 -0300

    added addtask in the handle submit of form and name state

--- client/src/components/tasks/FormTask.js
+++ client/src/components/tasks/FormTask.js
@@ -1,22 +1,34 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 import projectContext from '../../context/projects/projectContext';
+import taskContext from '../../context/tasks/taskContext';
 
 function FormTask() {
 
+  const [name, setName] = useState('');
+
   const { project } = useContext(projectContext);
+  const { addTask } = useContext(taskContext);
 
   if(!project) return null;
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    addTask(project.id, name);
+    setName('');
+  }
+
   return (
     <div className="form">
-      <form>
+      <form onSubmit={handleSubmit} >
         <div className="container-input">
           <input 
             type="text"
             name="name"
             className="input-text"
             placeholder="Task Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
         <div className="container-input">
