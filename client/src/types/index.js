9ccdb37791d358a40846a4fcb9e1d871e15aee77
Fedeya <elfedeminaya@gmail.com>
2020-03-31 20:33:46 -0300

    added state task type

--- client/src/types/index.js
+++ client/src/types/index.js
@@ -7,3 +7,4 @@ export const DELETE_PROJECT = 'DELETE_PROJECT';
 export const TASKS_PROJECT = 'TASKS_PROJECT';
 export const ADD_TASK = 'ADD_TASK';
 export const DELETE_TASK = 'DELETE_TASK';
+export const STATE_TASK = 'STATE_TASK';
