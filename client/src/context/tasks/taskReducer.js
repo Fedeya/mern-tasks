33ad84e7622dfde8940128ed7d5349800a40431a
Fedeya <elfedeminaya@gmail.com>
2020-03-31 19:54:26 -0300

    added delete task reducer

--- client/src/context/tasks/taskReducer.js
+++ client/src/context/tasks/taskReducer.js
@@ -1,4 +1,4 @@
-import { TASKS_PROJECT, ADD_TASK } from '../../types';
+import { TASKS_PROJECT, ADD_TASK, DELETE_TASK } from '../../types';
 
 export default (state, action) => {
   switch(action.type) {
@@ -12,6 +12,12 @@ export default (state, action) => {
         ...state,
         tasks: [...state.tasks, action.payload],
         projectTasks: [...state.projectTasks, action.payload]
+      }
+    case DELETE_TASK:
+      return {
+        ...state,
+        tasks: state.tasks.filter(task => task.id !== action.payload),
+        projectTasks: state.projectTasks.filter(task => task.id !== action.payload)
       }
     default:
       return state;
