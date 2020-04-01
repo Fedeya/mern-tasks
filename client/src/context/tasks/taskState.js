ffc168279be32363b341e1c4516c9981b22d5391
Fedeya <elfedeminaya@gmail.com>
2020-03-31 21:09:05 -0300

    added action of edit task, active task and clean task

--- client/src/context/tasks/taskState.js
+++ client/src/context/tasks/taskState.js
@@ -4,7 +4,7 @@ import * as uuid from 'uuid';
 import TaskContext from './taskContext';
 import TaskReducer from './taskReducer';
 
-import { TASKS_PROJECT, ADD_TASK, DELETE_TASK, STATE_TASK } from '../../types';
+import { TASKS_PROJECT, ADD_TASK, DELETE_TASK, STATE_TASK, ACTIVE_TASK, EDIT_TASK, CLEAN_TASK } from '../../types';
 
 function TaskState(props) {
   const initialState = {
@@ -14,7 +14,8 @@ function TaskState(props) {
       {id: 3, name: 'Select Payment Platform', state: false, projectId: 2},
       {id: 4, name: 'Select Hosting', state: true, projectId: 3}
     ],
-    projectTasks: null
+    projectTasks: null,
+    task: null
   };
 
   const [state, dispatch] = useReducer(TaskReducer, initialState);
@@ -47,14 +48,38 @@ function TaskState(props) {
     })
   }
 
+  const activeTask = task => {
+    dispatch({
+      type: ACTIVE_TASK,
+      payload: task
+    });
+  }
+
+  const editTask = name => {
+    dispatch({
+      type: EDIT_TASK,
+      payload: { ...state.task, name }
+    });
+  }
+
+  const cleanTask = () => {
+    dispatch({
+      type: CLEAN_TASK
+    });
+  } 
+
   return (
     <TaskContext.Provider
       value={{
         tasks: state.projectTasks,
+        task: state.task,
         getTasks,
         addTask,
         deleteTask,
-        stateTask
+        stateTask,
+        activeTask,
+        editTask,
+        cleanTask
       }}
     >
       {props.children}
