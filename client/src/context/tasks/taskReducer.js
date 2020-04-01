3e0d62c656d104b25ec9a66a386977105af1124d
Fedeya <elfedeminaya@gmail.com>
2020-03-31 21:09:20 -0300

    added reducer of edit task, active task and clean task

--- client/src/context/tasks/taskReducer.js
+++ client/src/context/tasks/taskReducer.js
@@ -1,4 +1,4 @@
-import { TASKS_PROJECT, ADD_TASK, DELETE_TASK, STATE_TASK } from '../../types';
+import { TASKS_PROJECT, ADD_TASK, DELETE_TASK, STATE_TASK, ACTIVE_TASK, EDIT_TASK, CLEAN_TASK } from '../../types';
 
 export default (state, action) => {
   switch (action.type) {
@@ -19,11 +19,22 @@ export default (state, action) => {
         tasks: state.tasks.filter(task => task.id !== action.payload),
         projectTasks: state.projectTasks.filter(task => task.id !== action.payload)
       }
+    case EDIT_TASK:
     case STATE_TASK:
       return {
         ...state,
         tasks: state.tasks.map(task => task.id === action.payload.id ? action.payload : task ),
-        projectTasks: state.projectTasks.map(task => task.id === action.payload.id ? action.payload : task )
+        projectTasks: state.projectTasks.map(task => task.id === action.payload.id ? action.payload : task ),
+      }
+    case ACTIVE_TASK:
+      return {
+        ...state,
+        task: action.payload
+      }
+    case CLEAN_TASK:
+      return {
+        ...state,
+        task: null
       }
     default:
       return state;
