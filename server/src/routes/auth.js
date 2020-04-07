d67486cbe01846f0c3066b31dd58e073ca56a426
Fedeya <elfedeminaya@gmail.com>
2020-04-07 18:06:31 -0300

    added newline

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const router = Router();
 const { check } = require('express-validator');
+
 const { authenticateUser } = require('../controllers/auth');
 
 router.route('/')
