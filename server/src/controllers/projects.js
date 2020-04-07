df4ee96e9ff0617f2fa4ed997b1904a6a4f495f0
Fedeya <elfedeminaya@gmail.com>
2020-04-07 20:58:56 -0300

    added get and update project controller

--- server/src/controllers/projects.js
+++ server/src/controllers/projects.js
@@ -15,7 +15,54 @@ exports.createProject = async (req, res) => {
     await project.save();
     res.json(project);
   } catch(err) {
-    res.status(400).json({
+    res.status(500).json({
+      msg: 'there was an error'
+    });
+  }
+
+}
+
+exports.getProjects = async (req, res) => {
+
+  try {
+    const projects = await Project.find({ author: req.user.id }).sort({
+      createdAt: -1
+    });
+    res.json(projects);  
+  } catch(err) {
+    res.status(500).json({
+      msg: 'there was an error'
+    });
+  }
+
+}
+
+exports.updateProject = async (req, res) => {
+
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({ erros: errors.array() });
+  }
+
+  const { name } = req.body;
+
+  try {
+    let project = await Project.findById(req.params.id);
+
+    if(!project) {
+      return res.status(404).json({ msg: 'project not found' });
+    }
+
+    if(project.author.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'not authorized' });
+    }
+
+    project = await Project.findByIdAndUpdate(req.params.id, { name }, { new: true });
+
+    res.json(project);
+    
+  } catch(err) {
+    res.status(500).json({
       msg: 'there was an error'
     });
   }
