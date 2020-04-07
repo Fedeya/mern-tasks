59b5bbecbc66e80eb8c2754c549c2f506daf647c
Fedeya <elfedeminaya@gmail.com>
2020-04-07 16:42:42 -0300

    added check to express validator in route

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,9 +1,14 @@
 const { Router } = require('express');
 const router = Router();
+const { check } = require('express-validator');
 
 const { addUser } = require('../controllers/users');
 
 router.route('/')
-  .post(addUser);
+  .post([ 
+    check('name', 'the name is required').not().isEmpty(),
+    check('email', 'add a valid email').isEmail(),
+    check('password', 'the password must be a minimum of 6 characters').isLength({ min: 6 })
+  ], addUser);
 
 module.exports = router;
