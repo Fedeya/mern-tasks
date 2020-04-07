8e5c47e4799cf937088bff92c0c0cd7725ff1311
Fedeya <elfedeminaya@gmail.com>
2020-04-07 20:58:36 -0300

    added get projects and update project route

--- server/src/routes/projects.js
+++ server/src/routes/projects.js
@@ -2,12 +2,18 @@ const { Router } = require('express');
 const router = Router();
 const { check } = require('express-validator');
 
-const { createProject } = require('../controllers/projects');
+const { createProject, getProjects, updateProject } = require('../controllers/projects');
 
 // api/projects
 router.route('/')
   .post([
     check('name', 'the name of project is required').not().isEmpty()
-  ], createProject);
+  ], createProject)
+  .get(getProjects)
+
+router.route('/:id')
+  .put([
+    check('name', 'the name of project is required').not().isEmpty()
+  ], updateProject)
 
 module.exports = router;
