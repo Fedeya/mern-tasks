a79659ba0f87e01ea2d499389f67af4ad47a392d
Fedeya <elfedeminaya@gmail.com>
2020-04-07 16:27:09 -0300

    created controller of user route

--- server/src/controllers/users.js
+++ server/src/controllers/users.js
@@ -1,5 +1,33 @@
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
 exports.addUser = async (req, res) => {
-  res.json({
-    user: 'created'
-  })
+  
+  const { name, email, password } = req.body;
+  
+  let user = await User.findOne({ email });
+  if(user) return res.json({ msg: 'user already exist' });
+
+  user = new User({
+    name, 
+    email,
+    password
+  });
+
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(password, salt);
+
+  try {
+    await user.save();
+
+    res.json({
+      msg: 'user created!'
+    });
+  
+  } catch(err) {
+    res.status(400).json({
+      msg: 'there was an error'
+    });
+  }
+
 }
