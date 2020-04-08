f3da5d7a166c13764d0ed8ce44da6c99f08d7541
Fedeya <elfedeminaya@gmail.com>
2020-04-07 21:24:40 -0300

    added delete route

--- server/src/routes/projects.js
+++ server/src/routes/projects.js
@@ -2,18 +2,19 @@ const { Router } = require('express');
 const router = Router();
 const { check } = require('express-validator');
 
-const { createProject, getProjects, updateProject } = require('../controllers/projects');
+const { createProject, getProjects, updateProject, deleteProject } = require('../controllers/projects');
 
 // api/projects
 router.route('/')
   .post([
     check('name', 'the name of project is required').not().isEmpty()
   ], createProject)
-  .get(getProjects)
+  .get(getProjects);
 
 router.route('/:id')
   .put([
     check('name', 'the name of project is required').not().isEmpty()
   ], updateProject)
+  .delete(deleteProject);
 
 module.exports = router;
