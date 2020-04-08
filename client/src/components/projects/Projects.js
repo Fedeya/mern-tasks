49c3576ac8416943a96684fdf05aab1ca4d3e5f1
Fedeya <elfedeminaya@gmail.com>
2020-04-08 19:38:29 -0300

    getting user authenticated

--- client/src/components/projects/Projects.js
+++ client/src/components/projects/Projects.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 
 import Sidebar from '../layout/Sidebar';
 import Header from '../layout/Header';
@@ -6,7 +6,17 @@ import Header from '../layout/Header';
 import FormTask from '../tasks/FormTask';
 import TaskList from '../tasks/TaskList';
 
+import AuthContext from '../../context/authentication/authContext';
+
 function Projects() {
+
+  const { userAuthenticated } = useContext(AuthContext);
+
+  useEffect(() => {
+    userAuthenticated();
+    // eslint-disable-next-line
+  }, [])
+
   return (
     <div className="container-app">
       <Sidebar />
