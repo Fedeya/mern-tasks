5b4c654699927c406a71e13e6d79e25cb65f18a2
Fedeya <elfedeminaya@gmail.com>
2020-04-08 18:31:12 -0300

    created new route to get user

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,13 +1,15 @@
 const { Router } = require('express');
 const router = Router();
 const { check } = require('express-validator');
+const auth = require('../middleware/auth');
 
-const { authenticateUser } = require('../controllers/auth');
+const { authenticateUser, userAuthenticated } = require('../controllers/auth');
 
 router.route('/')
   .post([
     check('email', 'add a valid email').isEmail(),
     check('password', 'the password must be a minimum of 6 characters').isLength({ min: 6 })
-  ], authenticateUser);
+  ], authenticateUser)
+  .get(auth, userAuthenticated);
 
 module.exports = router;
