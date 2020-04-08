b305ef2bc20323cef1e4c93afbb63469d51b1710
Fedeya <elfedeminaya@gmail.com>
2020-04-08 19:36:57 -0300

    added name of the user and logout function

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import AuthContext from '../../context/authentication/authContext';
 
 function Header() {
+
+  const { user, logOut } = useContext(AuthContext);
+
   return (
     <header className="app-header">
-      <p className="name-user">Hello <span>Federico</span></p>
+      <p className="name-user">Hello <span>{user && user.name}</span></p>
       <nav className="nav-main">
-        <a href="#!">Log Out</a>
+        <button 
+          className="btn btn-blank close-session"
+          onClick={logOut}
+        >
+          Log Out
+        </button>
       </nav>
     </header>
   );
