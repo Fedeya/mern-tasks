7c2c335eb9e91b1d72011fc387505de0f8f0c2f0
Fedeya <elfedeminaya@gmail.com>
2020-04-09 18:07:18 -0300

    changed put route

--- server/src/routes/tasks.js
+++ server/src/routes/tasks.js
@@ -6,16 +6,15 @@ const { check } = require('express-validator');
 
 // api/tasks
 router.route('/')
-  .get(getTasks)
   .post([
     check('name', 'the name of task is required').not().isEmpty(),
     check('project', 'the project is required').not().isEmpty()
   ], createTask)
+  .get(getTasks);
 
 router.route('/:id')
   .delete(deleteTask)
   .put([ 
-    check('name', 'the name of task is required').not().isEmpty(),
     check('project', "the project is required").not().isEmpty()
   ], updateTask)
 
